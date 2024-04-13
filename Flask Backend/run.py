fbdcd7a9c00c90ad7d2502a750b1aef98e09e30a
Sugandh-Mishra <77919928+Sugandh-Mishra@users.noreply.github.com>
2024-04-13 23:49:27 +0530

    Update run.py

--- Flask Backend/run.py
+++ Flask Backend/run.py
@@ -1,6 +1,12 @@
-from ML import app
+from flask import Flask
+from ML.routes.img_to_generate_image import img_to_generate_image_bp
+from ML.routes.denoise_image import denoise_image_bp
+from ML.routes.adversarial_attacks import adversarial_attacks_bp
+app = Flask(__name__)
 
-
-if __name__=="__main__":
+app.register_blueprint(img_to_generate_image_bp, url_prefix='/api')
+app.register_blueprint(denoise_image_bp, url_prefix='/api')
+app.register_blueprint(adversarial_attacks_bp,url_prefix='/api')
+app.register_blueprint(img_to_generate_image_bp,url_prefix='/api')
+if __name__ == '__main__':
     app.run(debug=True)
-    
